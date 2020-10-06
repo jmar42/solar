28627b913d668e76f6cbb19a9c22c7fe664526bd
Marcel Ebert <mail@marcel-ebert.de>
2020-10-06 11:33:25 +0200

    Rename AccountName to Address

--- src/Assets/components/AddAssetDialog.tsx
+++ src/Assets/components/AddAssetDialog.tsx
@@ -18,7 +18,7 @@ import { assetRecordToAsset, stringifyAsset } from "~Generic/lib/stellar"
 import { createTransaction } from "~Generic/lib/transaction"
 import { CompactDialogTransition } from "~App/theme"
 import DialogBody from "~Layout/components/DialogBody"
-import { AccountName } from "~Generic/components/AccountName"
+import Address from "~Generic/components/Address"
 import { SearchField } from "~Generic/components/FormFields"
 import { VerticalLayout } from "~Layout/components/Box"
 import { FixedSizeList } from "~Generic/components/VirtualList"
@@ -145,7 +145,7 @@ function createSearchResultRow(
         {item.type === "issuer" ? (
           <ListItem key={item.issuer} className={classes.issuerItem}>
             <ListItemText
-              primary={<AccountName address={item.issuer} testnet={account.testnet} />}
+              primary={<Address address={item.issuer} testnet={account.testnet} />}
               secondary={
                 assetsByIssuer[item.issuer].length === 1
                   ? t("account.add-asset.item.issuer.secondary.one-asset")
